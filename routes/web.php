fae4ad6c712444414906e258f00dde1a86d1dd30
vitormelods <vmsvitor20@gmail.com>
2025-12-02 21:29:19 -0300

    feat: redirect to panel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 // Rotas para pacientes (interface simplificada)
 Route::middleware(['web'])->group(function () {
+    Route::get('/', [\App\Http\Controllers\RedirectController::class, 'index']);
     Route::get('/checkin', [CheckinController::class, 'index'])->name('checkin.index');
     Route::post('/checkin', [CheckinController::class, 'store'])->name('checkin.store');
     Route::get('/checkin/success', [CheckinController::class, 'success'])->name('checkin.success');
