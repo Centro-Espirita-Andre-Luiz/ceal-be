fb283f4a9de54d1404b1092ec69ad7c11f683bd7
Joberval <29413697+jobervalmagalhaes@users.noreply.github.com>
2025-12-03 13:52:49 -0300

    Tests: add root route '/' returning welcome view to satisfy ExampleTest expecting 200

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 // Rotas para pacientes (interface simplificada)
 Route::middleware(['web'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('welcome');
     Route::get('/checkin', [CheckinController::class, 'index'])->name('checkin.index');
     Route::post('/checkin', [CheckinController::class, 'store'])->name('checkin.store');
     Route::get('/checkin/success', [CheckinController::class, 'success'])->name('checkin.success');
