fbaa41209ae83798bc73d150669a0d9abf86bfd4
andreoluiz <andrecardosoal99@gmail.com>
2025-12-04 03:33:30 -0300

    test ci

--- tests/Unit/PatientRelationshipsTest.php
+++ tests/Unit/PatientRelationshipsTest.php
@@ -70,6 +70,6 @@ class PatientRelationshipsTest extends TestCase
 
         $this->assertCount(2, $patient->absences);
         $this->assertInstanceOf(Absence::class, $patient->absences->first());
-        //testCi
+        //testCii
     }
 }
